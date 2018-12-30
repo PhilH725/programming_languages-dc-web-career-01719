f4e257f72e4e712760eb4005a0151e36a5eb791e
Phil Hartley <philh725@gmail.com>
2018-12-30 17:54:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |style, language|
-    new_hash[language]
+    new_hash[] = language
   end
   puts new_hash
 end
