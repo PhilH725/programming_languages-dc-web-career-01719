2a2172005a72c0162bc8af836f40ae3dcf0576ea
Phil Hartley <philh725@gmail.com>
2018-12-30 18:21:30 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,14 +1,3 @@
-#def reformat_languages(languages)
-#  # your code here
-#  new_hash = {}
-#  languages.each do |style, language_hash|
-#    language_style = []
-#    language_hash.each do |language_name, language_att_hash|
-#      new_hash[language_name] = language_att_hash
-#    end
-#  end
-#  puts new_hash
-#end
 
 def reformat_languages(languages)
   new_hash = {}
@@ -21,6 +10,5 @@ def reformat_languages(languages)
         new_hash[language_name] = {:type => language_att_hash[:type], :style => [style]}
     end
   end
-  puts new_hash
   new_hash
 end
